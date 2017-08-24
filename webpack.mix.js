9eb85d926a1c3502909a9658876d8e180776807c
Timon Lukas <timon.lukas3@gmail.com>
2017-08-24 10:13:40 +0200

    Fix css commands

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
 mix.js("resources/assets/js/util.js", "public/js")
-    .copy("resources/assets/css/util.css", "public/css")
-    .copy("resources/assets/css/main.css", "public/css");
+    .styles([
+        "resources/assets/css/util.css",
+        "resources/assets/css/main.css",
+    ], "public/css/style.css");
